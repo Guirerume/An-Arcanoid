e1bca75e68587aa4162816fe3480a04f40de0b8c
Tiago Paes <Tiago Paes>
2017-01-28 19:17:20 -0200

    add function start game and pause game

--- js/controller/Main.js
+++ js/controller/Main.js
@@ -3,24 +3,50 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 var ball = new Ball(canvas.width / 2 - 15, canvas.height - 60, 30, 30, 1);
 var player = new Player(canvas.width /2 - 50, 370, 20, 100, 5);
+var start = false;
+var pause = false;
 
 var keyboard = {};
 eventKeyboard(keyboard);
 
-function play() {
+var startBall = function(keyboard) {
+	if(13 in keyboard){
+		if(!start){
+			start = true;
+		}else if(!pause){
+			pause = true;
+		}else if(pause){
+			pause = false;
+		}
+	}
+};
+
+function play(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	var update = ball.moviment(player);
-	var updatePlayer = player.movement(keyboard);
-	drawBall(ball, "#00ff00");
-	drawPlayer(player, "blue");	
-
-	if(!update){
-
+	if(!start){
+		ball.x = player.x + 35;
+		var updatePlayer = player.movement(keyboard);
+		drawBall(ball, "#00ff00");
+		drawPlayer(player, "blue");
+		startBall(keyboard);
 		setTimeout(play, 7);
-	}	
+	}else if(!pause){
+		var update = ball.moviment(player);
+		var updatePlayer = player.movement(keyboard);
+		drawBall(ball, "#00ff00");
+		drawPlayer(player, "blue");
+		startBall(keyboard);
+		if(!update){
+			setTimeout(play, 7);
+		}
+	}else if(pause){
+		drawBall(ball, "#00ff00");
+		drawPlayer(player, "blue");
+		startBall(keyboard);
+		setTimeout(play, 7);		
+	}
 }
-
 play();
 
 
