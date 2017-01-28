73f8198ef9cb15f020213127050ef45bc3bb5030
Tiago Paes <Tiago Paes>
2017-01-28 11:39:20 -0200

    function to check if the ball exceds the player

--- js/controller/Main.js
+++ js/controller/Main.js
@@ -5,15 +5,20 @@ var ctx = canvas.getContext("2d");
 var ball = new Ball(canvas.width / 2 - 15, canvas.height - 60, 30, 30, 1);
 var player = new Player(canvas.width /2 - 50, 370, 20, 100, 10);
 
-function update() {
+function play() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	ball.moviment(player);
+	var update = ball.moviment(player);
 
 	drawBall(ball, "#00ff00");
 	drawPlayer(player, "blue");
+
+	if(!update){
+		setTimeout(play, 7);
+	}
 }
 
-setInterval(update, 10);
+play();
+
 
 
--- js/model/Ball.js
+++ js/model/Ball.js
@@ -23,5 +23,55 @@ var Ball = function (x, y, height, width, speed) {
 
 		this.x += (this.speed + this.mod) * this.dirx;
 		this.y += (this.speed + this.mod) * this.diry;
+
+		return this.exceedUser();
 	};
+
+	this.exceedUser = function(){
+		if(this.y + this.height - (this.height / 2) >= canvas.height){
+			return true;
+		}
+		return false;
+	};
+};
+
+
+function movebola(){
+	if(bola.y + bola.altura >= esquerda.y && bola.y <= esquerda.y + esquerda.altura && bola.x <= esquerda.x + esquerda.largura){
+		bola.dirx = 1;
+		bola.mod += 0.2;
+
+	}else if(bola.y + bola.altura >= direita.y && bola.y <= direita.y + direita.altura && bola.x + bola.largura >= direita.x){
+		bola.dirx = -1;
+		bola.mod += 0.2;
+	}
+
+	if(bola.y <= 0){
+		bola.diry = 1;
+	}else if(bola.y + bola.altura >= canvas.height){
+		bola.diry = -1;
+	}
+
+	bola.x += (bola.speed + bola.mod) * bola.dirx;
+	bola.y += (bola.speed + bola.mod) * bola.diry;
+
+	if(bola.x < esquerda.x + esquerda.largura -15){
+		newgame("player 2");
+	}else if(bola.x + bola.largura > direita.x + 15){
+		newgame("player 1");
+	}
+}
+
+function newgame(winner){
+	if(winner == "player 1"){
+		esquerda.score++;
+	}else{
+		direita.score ++;
+	}
+
+	esquerda.y = canvas.height / 2 - esquerda.altura / 2;
+	direita.y = esquerda.y;
+	bola.y = canvas.height / 2 - bola.altura / 2;
+	bola.x = canvas.width / 2 - bola.largura/ 2;
+	bola.mod = 0;
 };
