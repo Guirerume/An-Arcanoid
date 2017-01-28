021cbc9b11c9fcee9625d78762d7033d155a34bb
Guirerume <gpaula@2mundos.net>
2017-01-28 01:33:55 -0200

    Create Player Character

---
+++ js/model/Player.js
@@ -0,0 +1,9 @@
+
+var Player = function(x, y, height, width, speed)
+{
+	this.x = x;
+	this.y = y;
+	this.height = height;
+	this.width = width;
+	this.speed = speed;
+};
--- js/view/View.js
+++ js/view/View.js
@@ -5,3 +5,10 @@ var drawBall = function (ball, color) {
 	ctx.fillStyle = color;
 	ctx.fillRect(ball.x, ball.y, ball.width, ball.height);
 };
+
+var drawPlayer = function (player, color)
+{
+	ctx.fillStyle = color;
+	ctx.fillRect(player.x, player.y, player.width, player.height);
+
+};
