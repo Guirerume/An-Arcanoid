af2cd677a140167ab597717feb956983cc46fc6a
Guirerume <gpaula@2mundos.net>
2017-01-28 17:18:14 -0200

    Create Player Controls.

--- js/model/Player.js
+++ js/model/Player.js
@@ -6,4 +6,25 @@ var Player = function(x, y, height, width, speed)
 	this.height = height;
 	this.width = width;
 	this.speed = speed;
+	this.movement = function(keyboard)
+	{		
+		if(37 in keyboard && this.x > 0)
+		{
+			
+			this.x -= this.speed;
+			console.log("chamou");
+		}
+
+		else if(39 in keyboard && this.x + this.width < canvas.width)
+		{
+			this.x += this.speed;
+		}
+
+		else
+		{
+
+		}	
+	}
+
 };
+
